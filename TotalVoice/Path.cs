e6962e66e27c38d91c831f6bc8ba4fe328e7a360
DiloWagner <diegowagner4@gmail.com>
2018-05-29 23:31:25 -0300

    Enviando classe Path

--- TotalVoice/Path.cs
+++ TotalVoice/Path.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace TotalVoice
 {
     public class Path
     {
+        private readonly List<Object> Paths;
 
+        public Path()
+        {
+            Paths = new List<object>();
+        }
+
+        public void Add(Object Value)
+        {
+            Paths.Add(Value);
+        }
+
+        public string GetPathString()
+        {
+            string spath = "";
+            for (int i = 0; i < Paths.Count; i++)
+            {
+                spath += "/" + Paths[i];
+            }
+            return spath;
+        }
     }
 }
