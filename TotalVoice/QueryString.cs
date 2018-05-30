04e20f1b191fc6a781b1c2808a84f8065b8f7516
DiloWagner <diegowagner4@gmail.com>
2018-05-29 23:24:22 -0300

    Enviando classe QueryString

--- TotalVoice/QueryString.cs
+++ TotalVoice/QueryString.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
 namespace TotalVoice
 {
     public class QueryString
     {
         private Dictionary<string, Object> Query;
+
+        public QueryString()
+        {
+            Query = new Dictionary<string, Object>();
+        }
+
+        public void Add(string Key, Object Value)
+        {
+            Query.Add(Key, Value);
+        }
+
+        public bool IsEmpty()
+        {
+            return Query.Count() == 0;
+        }
+
+        public string Build()
+        {
+            return "?" + HttpUtility.UrlEncode(string.Join("&", Query.Select(Item => string.Format("{0}={1}", Item.Key, Item.Value))));
+        }
     }
 }
