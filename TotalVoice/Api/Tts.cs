086265763f5e27ec59afa3ed78cc6a646963565b
DiloWagner <diegowagner4@gmail.com>
2018-06-19 18:49:22 -0300

    Enviando TTS class

---
+++ TotalVoice/Api/Tts.cs
@@ -0,0 +1,40 @@
+﻿namespace TotalVoice.Api
+{
+    public class Tts : Api
+    {
+        public const string ROTA_TTS = "tts";
+
+        public Tts(TotalVoiceClient Client) : base(Client)
+        {
+        }
+
+        public string Enviar(IParameters parameters)
+        {
+            Path path = new Path();
+            path.Add(ROTA_TTS);
+
+            _request.SetPath(path);
+            return _client.Post(_request);
+        }
+
+        public string Buscar(int Id)
+        {
+            Path path = new Path();
+            path.Add(ROTA_TTS);
+            path.Add(Id);
+
+            _request.SetPath(path);
+            return _client.Get(_request);
+        }
+
+        public string Relatorio(IParameters parameters)
+        {
+            Path path = new Path();
+            path.Add(ROTA_TTS);
+            path.Add("relatorio");
+
+            _request.SetPath(path);
+            return _client.Get(_request);
+        }
+    }
+}
