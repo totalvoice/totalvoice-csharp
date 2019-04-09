04c1107d38d022b52a0c22c11745ce99f3cf4f93
DiloWagner <diegowagner4@gmail.com>
2019-04-08 21:03:38 -0300

    Enviando Webphone class

---
+++ TotalVoice/Api/Webphone.cs
@@ -0,0 +1,29 @@
+﻿
+namespace TotalVoice.Api
+{
+    /// <summary>
+    /// Webphone Class
+    /// https://totalvoice.github.io/totalvoice-docs/#webphone
+    /// </summary>
+    public class Webphone : Api
+    {
+        public const string ROTA_WEBPHONE = "webphone";
+
+        public Webphone(IClient client) : base(client) { }
+        public Webphone(IClient client, IRequest request) : base(client, request) { }
+
+        ///
+        /// <summary>
+        /// Requisita a URL do webphone de um ramal
+        /// </summary>
+        /// 
+        public string Url()
+        {
+            Path path = new Path();
+            path.Add(ROTA_WEBPHONE);
+
+            _request.SetPath(path);
+            return _client.SendRequest(_request, GET);
+        }
+    }
+}
